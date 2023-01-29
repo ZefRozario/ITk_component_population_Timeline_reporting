5798f9484edfe071b16d363ca1ecf617ebe934d5
Zefran Rozario <97534206+ZefRozario@users.noreply.github.com>
2023-01-29 18:13:59 +0100

    Update app.py

--- st/streamlit/app.py
+++ st/streamlit/app.py
@@ -14,10 +14,10 @@ col1, col2, col3 = st.columns(3)
     
 with st.form('Authorisation1'):
     with col1:
-        aut1=st.text_input('ITkdb Authentication code1',type="password")
+        aut1=st.text_input('ITkdb Authentication Code 1:',type="password")
     
     with col2:
-        aut2=st.text_input('ITkdb Authentication code2',type="password")
+        aut2=st.text_input('ITkdb Authentication Code 2:',type="password")
     st.form_submit_button('First Button', on_click=set_stage, args=(1,))
     if st.session_state.stage > 0:
         y=funk.user_aut_itkdb(aut1,aut2)
@@ -25,12 +25,12 @@ with st.form('Authorisation1'):
         selected_locations = st.multiselect("Institute Codes:",x)
         st.form_submit_button('Second Button', on_click=set_stage, args=(2,))
     if st.session_state.stage > 1:
-        st.write('step1')
+        #st.write('step1')
         funk.influxClient()
         charts=[]
         for i in range(len(selected_locations)):
         #charts=[]
-            st.write('Making graph'+str(i))
+            st.write('Making graph for institute '+str(selected_locations[i]))
 
 
             funk.getbuckets(selected_locations[i])
